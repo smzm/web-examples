ccb93d6cc42b2e30d4cc79686d31225d5f459d3f
Saeed <39596095+smzm@users.noreply.github.com>
2021-09-20 09:16:47 -0400

    Add files via upload

--- Journal/settings.py
+++ Journal/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'django_htmx',
     'dashboard.apps.DashboardConfig',
     'users.apps.UsersConfig',
 
@@ -84,6 +85,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django_htmx.middleware.HtmxMiddleware',
 ]
 
 REST_FRAMEWORK = {
