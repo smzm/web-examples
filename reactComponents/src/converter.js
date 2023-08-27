3462c8faad490961f3ee3ef4a848e3e9992d2527
smzm <39596095+smzm@users.noreply.github.com>
2023-08-27 12:02:16 +0330

    Update converter.js

--- reactComponents/src/converter.js
+++ reactComponents/src/converter.js
@@ -1,51 +1,57 @@
 import { useEffect, useState } from "react";
 
-// App Component
-export default function App() {
+export default function Converter({ currencies }) {
 	const [amount, setAmount] = useState(1);
-	const [base, setBase] = useState("USD");
-	const [quote, setQuote] = useState("EUR");
+	const [base, setBase] = useState(function () {
+		return currencies[0];
+	});
+	const [quote, setQuote] = useState(function () {
+		return currencies[1];
+	});
 	const [ratio, setRatio] = useState();
 
 	function handleBase(e) {
 		const base = e.target.value;
+
+		// If [[base]] currency changed to what [[quote]] currency is, change [[quote]] to what was [[base]] before
 		if (base === "USD" && quote === "USD") {
-			setQuote("EUR");
+			setQuote(localStorage.getItem("base"));
 		}
 		if (base === "EUR" && quote === "EUR") {
-			setQuote("USD");
+			setQuote(localStorage.getItem("base"));
 		}
 		if (base === "GBP" && quote === "GBP") {
-			setQuote("USD");
+			setQuote(localStorage.getItem(""));
 		}
 		setBase(e.target.value);
 	}
 
 	function handleQuote(e) {
 		const quote = e.target.value;
+		// If [[quote]] currency changed to what [[base]] currency is, change [[base]] to what was [[quote]] before
 		if (base === "USD" && quote === "USD") {
-			setBase("EUR");
+			setBase(localStorage.getItem("quote"));
 		}
 		if (base === "EUR" && quote === "EUR") {
-			setBase("USD");
+			setBase(localStorage.getItem("quote"));
 		}
 		if (base === "GBP" && quote === "GBP") {
-			setBase("USD");
+			setBase(localStorage.getItem("quote"));
 		}
 		setQuote(e.target.value);
 	}
 
 	function handleAmount(e) {
-		console.log(e.target.value);
 		e.preventDefault();
+
+		// if [[amount input]] isn't number data type
 		if (isNaN(Number(e.target.value))) return;
-		if (e.target.value == 0) {
-			console.log(e.target.value);
-			return setAmount(0);
-		}
+
+		// set [[amount]] state
 		setAmount(Number(e.target.value));
 	}
 
+	// pressing "Escape" to turning back text input to 1
 	useEffect(function () {
 		function callback(e) {
 			if (e.code === "Escape") {
@@ -58,19 +64,30 @@ export default function App() {
 		};
 	}, []);
 
+	// fetch data from API whenever [[amount input]], [[base currency]] and [[quote currency]] changed
 	useEffect(
 		function () {
 			const controller = new AbortController();
 			async function FetchRatio() {
 				try {
+					// if [[amount input]] is zero
+					if (amount === 0) {
+						// 	console.log(e.target.value);
+						setRatio(0);
+						return;
+					}
+
 					const response = await fetch(
 						`https://api.frankfurter.app/latest?amount=${amount}&from=${base}&to=${quote}`,
 					);
 					const data = await response.json();
+
+					// Catch Error with status code 422 from API
 					if (response.status === 422) {
 						console.log(data);
 						throw new Error("zeroAmount");
 					}
+
 					const ratio = data.rates[quote];
 					setRatio(ratio);
 				} catch (err) {
@@ -87,22 +104,32 @@ export default function App() {
 		[base, quote, amount],
 	);
 
+	// set a local [[key]] : [[value]] everytime [[base]] and [[quote]] change
+	useEffect(
+		function () {
+			localStorage.setItem("base", base);
+			localStorage.setItem("quote", quote);
+		},
+		[base, quote],
+	);
+
 	return (
 		<div>
-			<form action="">
-				<input type="text" value={amount} onChange={(e) => handleAmount(e)} />
-				<select name="" id="" value={base} onChange={(e) => handleBase(e)}>
-					<option value="USD">USD</option>
-					<option value="EUR">EUR</option>
-					<option value="GBP">GBP</option>
-				</select>
-				<select name="" id="" value={quote} onChange={(e) => handleQuote(e)}>
-					<option value="USD">USD</option>
-					<option value="EUR">EUR</option>
-					<option value="GBP">GBP</option>
-				</select>
-			</form>
-			<p> {ratio} </p>
+			<input type="text" value={amount} onChange={(e) => handleAmount(e)} />
+			<select name="" id="" value={base} onChange={(e) => handleBase(e)}>
+				<option value="USD">USD</option>
+				<option value="EUR">EUR</option>
+				<option value="GBP">GBP</option>
+			</select>
+			<select name="" id="" value={quote} onChange={(e) => handleQuote(e)}>
+				<option value="USD">USD</option>
+				<option value="EUR">EUR</option>
+				<option value="GBP">GBP</option>
+			</select>
+			<p>
+				{" "}
+				{ratio} {quote}
+			</p>
 		</div>
 	);
 }
